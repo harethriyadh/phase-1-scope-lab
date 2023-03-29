3b8e4100efb9ef36b3307900e62011aa3af3c3f0
harith <hareth.cn12012@gmail.com>
2023-03-29 23:47:10 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+
+var customerName = "bob";
+
+function getCustomerName (  )
+{
+    return customerName ;
+}
+function upperCaseCustomerName()
+{
+    customerName = customerName.toUpperCase();
+    
+    return customerName ;
+}
+
+
+var bestCustomer ;
+
+function setBestCustomer (  ) {
+    bestCustomer = "not bob" ;
+    return bestCustomer ;
+}
+
+function overwriteBestCustomer()
+{
+    bestCustomer = "maybe bob" ;
+    return bestCustomer ;
+}
+
+const leastFavoriteCustomer = "something" ;
+function  changeLeastFavoriteCustomer()
+{
+    leastFavoriteCustomer = "Hi" ;
+}
